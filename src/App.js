9e9592c563f055d55969ea7eb64280ea53fe7a37
Shohruh003 <shohruhazimov003@gmail.com>
2022-08-05 01:36:11 +0500

    get init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./app.scss";
+import {List} from "./components";
+import {ListItem} from "./components";
 
 function App() {
+
+  const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem("todos")) || []);
+
+  const handleInputValue = (evt) => {
+    if (evt.code === "Enter") {
+      const newTodo = {
+        id: todos.at(-1)?.id ? todos.at(-1).id + 1 : 1,
+        text: evt.target.value,
+        isComplated: false
+      }
+
+      setTodos([...todos,newTodo]);
+      evt.target.value = "";
+    }
+  }
+
+  window.localStorage.setItem("todos", JSON.stringify(todos))
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp={handleInputValue} type="text" placeholder="Todo..."/>
+      {todos.length > 0 && <List>
+        {
+          todos.map(e => (
+            <ListItem key={e.id} item={e} todos={todos} setTodos={setTodos}/>
+          ))
+        }
+      </List>}
     </div>
   );
 }
